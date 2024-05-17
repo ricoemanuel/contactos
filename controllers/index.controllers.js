a20befbc86028e1ce435e2435892b321a9ef5b44
ricoemanuel <ricoruizemanuel@gmail.com>
2024-05-17 15:14:11 -0500

    post method

--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -1,7 +1,20 @@
+require('dotenv').config();
+const url=process.env.URL
 const getUsers=async (req,res)=>{
-    const response=await fetch('https://crud-contactos-iefo-default-rtdb.firebaseio.com/users.json')
+    const response=await fetch(`${url}/users.json`)
     const data=await response.json()
     res.send(data)
 }
+const postUsers=async (req,res)=>{
+    const newuser=req.body
+    const response=await fetch(`${url}/users.json`,{
+        method:"POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(newuser)
+    })
+    res.send(response)
+}
 
-module.exports={getUsers}
+module.exports={getUsers,postUsers}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const {Router}=require('express')
-const {getUsers}=require('../controllers/index.controllers')
+const {getUsers,postUsers}=require('../controllers/index.controllers')
 const router=Router();
 
 router.get('/users',getUsers)
+router.post('/users',postUsers)
 module.exports=router
